e55d2d68678952a9a9840f47d6d516917a09e193
David Riveraperez <david.i.riveraperez@gmail.com>
2020-01-11 10:47:00 -0600

    Server Working

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,10 @@ var path = require("path");
 var app = express();
 
 var fs = require("fs");
+var notes= require("./db/db.json");
 
+
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -17,50 +20,46 @@ var PORT = 3000;
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
-    console.log(req);
+    // console.log(req);
 });
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
-    console.log(req);
+    // console.log(req);
 });
 
 // Displays all characters
 app.get("/api/notes", function (req, res) {
-    readNotes(function (data) {
-        console.log(req);
-        res.json(data);
-    })
+    // readNotes(function (data) {
+        res.json(notes);
+        console.log(notes);
+    
 });
 
-function readNotes(callback) {
-    fs.readFile("db/db.json", "utf8", function (error, data) {
-        if (error) {
-            return console.log(error);
-        }
-        return callback(JSON.parse(data));
-    });
-}
+// function readNotes(callback) {
+//     fs.readFile("db/db.json", "utf8", function (error, data) {
+//         if (error) {
+//             return console.log(error);
+//         }
+//         return callback(JSON.parse(data));
+//     });
+// }
 
 
-function writeNotes(notes, callback) {
-    fs.writeFile("db/db.json", JSON.stringify(notes), function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        callback();
-    });
-};
+// function writeNotes(notes, callback) {
+//     fs.writeFile("db/db.json", JSON.stringify(notes), function (err) {
+//         if (err) {
+//             return console.log(err);
+//         }
+//         callback();
+//     });
+// };
 
 app.post("/api/notes", function (req, res) {
-    readNotes(function (data) {
-        console.log(req.body);
-        var newNote = req.body;
-        data.push(newNote);
-        writeNotes(data, function () {
-            res.send("success");
-        });
-    });
+    var newNote = req.body;
+    console.log(newNote);
+        // data.push(newNote);
+
 });
 
 app.delete("/api/notes/:id", function (req, res) {
